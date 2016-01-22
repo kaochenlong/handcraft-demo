55816098e96b8d63e39f92f239f3a9c33af39ff8
Eddie Kao <eddie@digik.com.tw>
2016-01-22 21:31:51 +0800

    add create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,17 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def create
+    @contact = Contact.new(params[:contact])
+
+    if @contact.save
+      # 成功
+      flash[:notice] = "已成功送出!"
+      redirect_to root_path
+    else
+      flash[:notice] = "失敗!!"
+      # 失敗
+    end
+  end
+
 end
