a4a7d738086c49f70dfc45b789d54973eba6c8e2
Eddie Kao <eddie@digik.com.tw>
2016-01-24 10:53:10 +0800

    add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get "/about", to: "static_pages#about"
   root "static_pages#home"
 
+  get "/about", to: "static_pages#about"
+
   resources :contacts, only: [:index, :create]
+  resources :posts
 
   namespace :admin do
     resources :contacts, except:[:new, :create, :destroy]
   end
-
 end
