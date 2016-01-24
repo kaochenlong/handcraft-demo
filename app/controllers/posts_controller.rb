fcb50de476540177681f6f15e79c11a58e50da3a
Eddie Kao <eddie@digik.com.tw>
2016-01-24 11:12:07 +0800

    add index page for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    flash[:notice] = "已成功送出!"
+    redirect_to posts_path
   end
 
   def edit
   end
 
   def show
+  end
+
+  # Strong Parameter
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122125937) do
+ActiveRecord::Schema.define(version: 20160124025417) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "subject"
     t.string   "email"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
